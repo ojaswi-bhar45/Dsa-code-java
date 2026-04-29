edfe0984bc71a3ca8879d8b906d0d0281815c617
Ojaswi bhardwaj <ojaswibhardwaj86@gmail.com>
2026-04-29 07:05:38 +0530

    To check the number is palindrome or not

---
+++ src/Palindrome/palindrom.java
@@ -0,0 +1,28 @@
+package Palindrome;
+
+import java.util.Scanner;
+
+public class palindrom {
+    public  static boolean isPalindrome(int x){
+        if ( x < 0){
+            return false;
+        }
+        int original = x;
+        int reversed = 0;
+
+        while( x != 0 ){
+            int digit = x % 10;
+            reversed = reversed * 10 + digit;
+            x /= 10;
+
+        }
+        return original==reversed;
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a number to check the palindrome");
+        int x = sc.nextInt();
+
+        System.out.println(isPalindrome(x));
+    }
+}
