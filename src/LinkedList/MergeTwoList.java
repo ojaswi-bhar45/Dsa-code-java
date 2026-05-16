12402e86b155e1c5fbeeb4ccaf16588fa3f0c602
Ojaswi bhardwaj <ojaswibhardwaj86@gmail.com>
2026-05-16 10:40:46 +0530

    Add the Remove_Duplicates_in_sorted_array

---
+++ src/LinkedList/MergeTwoList.java
@@ -0,0 +1,14 @@
+//package LinkedList;
+//
+//import java.util.Arrays;
+//import java.util.Scanner;
+//
+//public class MergeTwoList {
+//     public <ListNode> ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+//
+//    }
+//
+//    public static void main(String[] args) {
+//
+//    }
+//}
---
+++ src/RemoveDuplicatesFromArray/remove_duplicates_from_sorted_array.java
@@ -0,0 +1,49 @@
+package RemoveDuplicatesFromArray;
+
+import java.util.Scanner;
+
+public class remove_duplicates_from_sorted_array {
+
+    public static int removeDuplicates(int[] nums) {
+
+        int i = 0;
+
+        for(int j = 1; j < nums.length; j++) {
+
+            if(nums[i] != nums[j]) {
+                i++;
+                nums[i] = nums[j];
+            }
+        }
+
+        return i + 1;
+    }
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter the size of array:");
+        int n = sc.nextInt();
+
+        int[] nums = new int[n];
+
+        System.out.println("Enter the elements in array:");
+
+        for(int i = 0; i < n; i++) {
+            nums[i] = sc.nextInt();
+        }
+
+        int k = removeDuplicates(nums);
+
+        System.out.println("Number of unique elements: " + k);
+
+        System.out.print("Updated Array: ");
+
+        for(int i = 0; i < k; i++) {
+            System.out.print(nums[i] + " ");
+        }
+
+        sc.close();
+    }
+}
