d67fc925183d53b07d8598cd0c7942118ad5641b
Ojaswi bhardwaj <ojaswibhardwaj86@gmail.com>
2026-05-31 20:55:42 +0530

    ques 33

--- src/TOP_LEETCODE_75_QUESTION/SearchInSortedArray.java
+++ src/TOP_LEETCODE_75_QUESTION/SearchInSortedArray.java
@@ -1,2 +1,60 @@
-package TOP_LEETCODE_75_QUESTION;public class SearchInSortedArray {
+package TOP_LEETCODE_75_QUESTION;
+
+import java.util.Scanner;
+
+public class SearchInSortedArray {
+
+    public static int search(int[] nums, int target) {
+        int left = 0;
+        int right = nums.length - 1;
+
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+
+            if (nums[mid] == target) {
+                return mid;
+            }
+
+            // Left half is sorted
+            if (nums[left] <= nums[mid]) {
+
+                if (target >= nums[left] && target < nums[mid]) {
+                    right = mid - 1;
+                } else {
+                    left = mid + 1;
+                }
+            }
+            // Right half is sorted
+            else {
+
+                if (target > nums[mid] && target <= nums[right]) {
+                    left = mid + 1;
+                } else {
+                    right = mid - 1;
+                }
+            }
+        }
+
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter the size of array: ");
+        int n = sc.nextInt();
+
+        int[] arr = new int[n];
+
+        System.out.println("Enter the elements of array: ");
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        System.out.println("Enter the target element: ");
+        int tar = sc.nextInt();
+
+        int idx = search(arr, tar);
+        System.out.println("Index = " + idx);
+    }
 }
