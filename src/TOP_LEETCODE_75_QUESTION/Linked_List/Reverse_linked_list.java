0a55dc11bccef806d4d0399cbd52c7ccb1549361
Ojaswi bhardwaj <ojaswibhardwaj86@gmail.com>
2026-07-28 17:02:33 +0530

    Add the linked list

---
+++ src/TOP_LEETCODE_75_QUESTION/Linked_List/Reverse_linked_list.java
@@ -0,0 +1,23 @@
+//https://leetcode.com/problems/reverse-linked-list/description/
+
+package TOP_LEETCODE_75_QUESTION.Linked_List;
+class Node{
+    int data;
+    Node next;
+
+    Node(int data){
+        this.data = data;
+        this.next = null;
+    }
+}
+public class Reverse_linked_list {
+    static Node reverse_list(Node head){
+
+
+    }
+
+    public static void main(String[] args) {
+
+    }
+
+}
